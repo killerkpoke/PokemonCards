d920e67f75dda9f042c989844942594e44b066fa
= <killerkpoke@gmail.com>
2022-08-22 12:58:59 -0400

    third commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,72 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
 
+import axios from "axios";
 export default defineComponent({
-  props: {
-    pokemon_name: String,
-    pokemon_ability_name: String,
-    pokemon_height: Number,
-    pokemon_weight: Number,
-    pokemon_moves: Array,
-    pokemon_stats: Array,
-    pokemon_types: Array,
+  data() {
+    const poke_url = "https://pokeapi.co/api/v2/pokemon/charizard";
+    axios
+      .get(poke_url)
+      .then((res) => {
+        const adat = ref(res.data);
+
+        this.pokemon_name = adat.value.name;
+        // pokemon_ability_name: [],
+        this.pokemon_height = adat.value.height;
+        this.pokemon_weight = adat.value.weight;
+        this.pokemon_stat_hp = adat.value.stats.hp;
+
+        //console.log(adat.value.stats); // 6 db statunk van        
+        //console.log(adat.value.stats[0].base_stat);
+        
+        //for(let i = 0; i < adat.value.stats.length; i++) { // computed method-ban kell szamolni, hogy keves maradjon a request.
+          console.log(adat.value.stats[1].base_stat);
+          
+       // }
+
+        
+        
+        // pokemon_moves: [],
+        // pokemon_stats: [],
+        // pokemon_types: [],
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return {
+      pokemon_name: this.pokemon_name,
+      pokemon_height: this.pokemon_height,
+      pokemon_weight: this.pokemon_weight,
+
+    };
   },
+  // computed() {
+
+  // }
 });
 </script>
 
 <template>
   <div class="card">
+    <!-- <ul v-if="!loading && data">
+      <li v-for="item of data" v-bind:key="item">
+        <p>
+          <strong>{{ item }}</strong>
+        </p>
+        <p></p>
+      </li>
+    </ul>
+  -->
+    <p>{{ pokemon_name }}</p>
+    <p>{{ pokemon_height }}</p>
+    <p>{{ pokemon_weight }}</p>
+    
+    <p ref="error">{{}}</p>
     <div class="card-top">
       <div id="pokemon-name">Charizard</div>
       <div id="stat-hp">120</div>
+      <div id="pokemon-type">Fire/Fly</div>
     </div>
     <div class="card-img">
       <!-- pokemon image -->
