cba5517a2ea4c56ffc47b8ed565b5539dd70f527
David Kohalmi <killerkpoke@gmail.com>
2022-11-01 11:23:18 +0100

    fetching kinda working

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,10 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import PokeApi from "@/services/PokeApi";
 import type Pokemon from "@/types/Pokemon";
 import type PokemonTM from "@/types/PokemonTM";
 
 export default defineComponent({
-  props: {
-    pokemon_name: String,
-  },
   data() {
     // const poke_url = "https://pokeapi.co/api/v2/pokemon/eevee";
     // const getRandomElement = (arr: any[]) => arr.length ? arr[Math.floor(Math.random() * arr.length)] : undefined;
@@ -23,7 +20,6 @@ export default defineComponent({
         .then((response) => {
           this.pokemon = response.data;
           console.log(this.pokemon);
-      
         })
         .catch((e: Error) => {
           console.log(e);
@@ -41,8 +37,7 @@ export default defineComponent({
     },
   },
   mounted() {
-    console.log("kocsog", this.$props.pokemon_name);
-    this.getPokemon(this.$props.pokemon_name);
+    this.getPokemon("eevee");
     //this.getPokemon("eevee"); //this.$route.params.name);
     this.getSkill(19);
   },
@@ -50,79 +45,92 @@ export default defineComponent({
 </script>
 
 <template>
-  <div
-    class="mx-auto md:mx-40 ring-8 ring-yellow-400 ring-offset-4 rounded-lg shadow-2xl shadow-white/25 bg-slate-100"
-  >
-    <div class="flex p-2 text-black text-lg">
-      <div id="pokemon-name" class="font-semibold flex-auto capitalize">
-        {{ pokemon.name }}
-      </div>
-      <div
-        v-for="(item, k) in pokemon.types"
-        :key="k"
-        id="pokemon-type"
-        class="font-semibold flex-none"
+  <main>
+    <div class="mb-12 grid justify-center">
+      <label
+        for="first_name"
+        class="block mb-2 text-lg text-center font-medium text-gray-900 dark:text-gray-300"
+        >Search a Pokemon</label
       >
-        [{{ item.type.name }}]
-      </div>
-    </div>
-    <div class="mt-1 mx-1 border border-black bg-black rounded-full"></div>
-    <div class="m-4 border-4 border-yellow-500/50 rounded-md">
-      <!-- pokemon image -->
-      <div
-        class="max-w-auto max-h-auto w-4/5 h-4/5 text-black"
-        v-for="(item, k) in pokemon.sprites"
-        :key="k"
-      >
-        <img
-          class="h-auto max-h-60 max-w-60 w-auto bg-contain"
-          alt="pokepics"
-          :src="pokemon.sprites.front_default"
-        />
-      </div>
-    </div>
-    <div class="text-black">
-      <div
-        id="moves"
-        class="grid grid-cols-2 grid-rows-2 text-center capitalize"
-      >
-        <!-- Attack -->
-        <!-- name, url.power, url.accuracy, url.type.name -->
-        {{}}
-        <div>
-          <div class="font-semibold">move name:</div>
-          <div class="font-semibold">{{ pokemonTM.name }}</div>
-        </div>
-        <div>
-          <div class="font-semibold">move power:</div>
-          <div class="font-semibold">{{ pokemonTM.power }}</div>
-        </div>
-        <div>
-          <div class="font-semibold">move accuracy:</div>
-          <div class="font-semibold">{{ pokemonTM.accuracy }}</div>
-        </div>
-        <div>
-          <div class="font-semibold">move pp:</div>
-          <div class="font-semibold">{{ pokemonTM.pp }}</div>
-        </div>
-      </div>
+      <input
+        type="text"
+        id="first_name"
+        class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+        placeholder="ex. Zapdos"
+        required
+        v-model="pokemon.name"
+        @change="getPokemon(pokemon.name)"
+      />
     </div>
     <div
-      id="divider"
-      class="m-2 border border-2 border-red-400 bg-red-400"
-    ></div>
-    <div class="text-black p-2">
-      <div class="grid grid-cols-3 grid-rows-3 text-center">
+      class="mx-auto md:mx-40 ring-8 ring-yellow-400 ring-offset-4 rounded-lg shadow-2xl shadow-white/25 bg-slate-100"
+    >
+      <div class="flex p-2 text-black text-lg">
+        <div id="pokemon-name" class="font-semibold flex-auto capitalize">
+          {{ pokemon.name }}
+        </div>
         <div
-          v-for="(item, k) in pokemon.stats"
+          v-for="(item, k) in pokemon.types"
           :key="k"
-          id="stats"
-          class="capitalize"
+          id="pokemon-type"
+          class="font-semibold flex-none"
         >
-          <strong>{{ item.stat.name }}</strong>
-          <div>{{ item.base_stat }}</div>
+          [{{ item.type.name }}]
+        </div>
+      </div>
+      <div class="mt-1 mx-1 border border-black bg-black rounded-full"></div>
+      <div class="m-4 border-4 border-yellow-500/50 rounded-md">
+        <!-- pokemon image -->
+        <div v-for="(item, k) in pokemon.sprites" :key="k">
+          <img
+            class="w-screen h-max bg-contain"
+            alt="pokepics"
+            :src="pokemon.sprites.front_default"
+          />
+        </div>
+      </div>
+      <div class="text-black">
+        <div
+          id="moves"
+          class="grid grid-cols-2 grid-rows-2 text-center capitalize"
+        >
+          <!-- Attack -->
+          <!-- name, url.power, url.accuracy, url.type.name -->
+          <div>
+            <div class="font-semibold">move name:</div>
+            <div class="font-semibold">{{ pokemonTM.name }}</div>
+          </div>
+          <div>
+            <div class="font-semibold">move power:</div>
+            <div class="font-semibold">{{ pokemonTM.power }}</div>
+          </div>
+          <div>
+            <div class="font-semibold">move accuracy:</div>
+            <div class="font-semibold">{{ pokemonTM.accuracy }}</div>
+          </div>
+          <div>
+            <div class="font-semibold">move pp:</div>
+            <div class="font-semibold">{{ pokemonTM.pp }}</div>
+          </div>
+        </div>
+      </div>
+      <div
+        id="divider"
+        class="m-2 border border-2 border-red-400 bg-red-400"
+      ></div>
+      <div class="text-black p-2">
+        <div class="grid grid-cols-3 grid-rows-3 text-center">
+          <div
+            v-for="(item, k) in pokemon.stats"
+            :key="k"
+            id="stats"
+            class="capitalize"
+          >
+            <strong>{{ item.stat.name }}</strong>
+            <div>{{ item.base_stat }}</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,33 +1,6 @@
 <script setup lang="ts">
-import Card from "../components/Card.vue";
-import { ref } from "vue";
-
-let pokename = ref<any | null>("zapdos")
-
-function search_pokemon(event: Event) {
-  pokename = (event.target as HTMLInputElement).value;
-  console.log("kocsog", pokename);
-}
-
+import CardVue from "../components/Card.vue";
 </script>
-
 <template>
-  <main>
-    <div class="mb-12 grid justify-center">
-      <label
-        for="first_name"
-        class="block mb-2 text-lg text-center font-medium text-gray-900 dark:text-gray-300"
-        >Search a Pokemon</label
-      >
-      <input
-        type="text"
-        id="first_name"
-        class="text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-        placeholder="ex. Zapdos"
-        required
-        @change="search_pokemon"
-      />
-    </div>
-    <Card :pokemon_name="pokename"/>
-  </main>
+  <CardVue />
 </template>
